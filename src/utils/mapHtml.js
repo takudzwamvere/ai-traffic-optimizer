1aa66729c47f2ca5f5c4fbb3594846ecaf2fa54c
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-09-10 20:14:30 +0200

    fix: use HTTPS for Google Maps tile layer URLs
    HTTP tile URLs fail on iOS due to App Transport Security and trigger
    mixed-content warnings on Android WebView. Switch both Google Streets
    and Google Hybrid layers to HTTPS.

--- src/utils/mapHtml.js
+++ src/utils/mapHtml.js
@@ -103,13 +103,13 @@ export const getMapHtml = (defaultCoords = DEFAULT_COORDS) => `
       var initLng = ${defaultCoords?.lon ?? 31.0522};
 
       // 1. Google Streets (The "Google-like" layer)
-      googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      googleStreets = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
       });
 
       // 2. Google Hybrid (Satellite + Labels)
-      googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+      googleHybrid = L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
       });
